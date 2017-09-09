f2c02d925a17499b65757d528618ae6be1dbaf21
Bala <bala.eduseva@gmail.com>
2017-09-09 14:20:38 +0530

    init repo

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+<title>Internal Marks</title>
+</head>
+<body>
+	<h4>Internal Marks</h4>
+	<h4>Student Name: Venkatapathy S</h4>
+	<table style="border: 1px solid black; width:50%" border="1px">
+		<tbody>
+			<tr>
+				<th>Subject</th>
+				<th>Marks</th>
+			</tr>
+			<tr>
+				<td>Programming, Data Structures II</td>
+				<td style='color:red'><b>3</b></td>
+			</tr>
+			<tr>
+				<td>Computer Architecture</td>
+				<td style='color:red'><b>2</b></td>
+			</tr>
+			<tr>
+				<td>Analog, Digital Comm</td>
+				<td style='color:red'><b>5</b></td>
+			</tr>
+			<tr>
+				<td>Environmental Science</td>
+				<td style='color:red'><b>6</b></td>
+			</tr>
+			<tr>
+				<td>DBMS</td>
+				<td style='color:red'><b>8</b></td>
+			</tr>
+		</tbody>
+	</table>
+</body>
+</html>
